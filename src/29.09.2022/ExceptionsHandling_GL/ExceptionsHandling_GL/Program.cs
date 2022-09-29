db000847bdce6d2ee4dcfecafe833781f5eb4389
programxo <programxo98@proton.me>
2022-09-29 21:32:47 +0200

    Grundlagen Exceptionshandling

---
+++ src/29.09.2022/ExceptionsHandling_GL/ExceptionsHandling_GL/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsHandling_GL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string eingabe = "08152661616";
+            int zahl = 0;
+
+            try
+            {
+                zahl = int.Parse(eingabe);
+                // zahl = int.TryParse(eingabe);
+
+                Console.WriteLine($"Zahl: {zahl}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Ungültige Eingabe: {ex.Message}");
+            }
+        }
+    }
+}
