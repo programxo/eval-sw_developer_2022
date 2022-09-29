0d6a775247616bebe63b2670144651164e231c6b
programxo <programxo98@proton.me>
2022-09-29 19:55:58 +0200

    Basics about dataTypes

--- src/29.09.2022/DatenTypen/DatenTypen/Program.cs
+++ src/29.09.2022/DatenTypen/DatenTypen/Program.cs
@@ -10,6 +10,36 @@ namespace DatenTypen
     {
         static void Main(string[] args)
         {
+            //Deklaration
+            int zahl;
+
+            //Console.WriteLine(zahl); == GEHT NICHT!
+
+            //Initialisierung
+            zahl = 5;
+
+            //Deklaration & Initialisierung
+            int zahl2 = 56;
+
+            //
+
+            Console.WriteLine(int.MinValue); // mit verschiedenen Datentypen möglich
+            Console.WriteLine(int.MaxValue); // mit verschiedenen Datentypen möglich
+
+            string vorName = "Tiger"; // Unbegrentzte Menge
+
+            vorName = ""; // Leerer String
+            vorName = string.Empty; // Leerer String mit Methode
+
+            bool isPowerOn = true;
+
+            float flt = 1.5f; // Mit "f" als float verständlich, nicht das genauste
+
+            double gewicht = 15.654655; // Sehr genau
+
+            decimal gehalt = 1654.321m; // Mit "m" als decimal verständlich, verbraucht sehr viel Power
+
+            char sign 'd'; 
 
         }
     }
